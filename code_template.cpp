3807295265148a78a02417efa1b843b4caceb217
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-01 00:42:12 +0900

    Add cpp template for problem-solving

---
+++ code_template.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+
+#define mp make_pair
+
+int main(void) {
+//  freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
+
+
+    return 0;
+}
